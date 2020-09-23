6440c6c1ef0c024e3084db5393c2709182edb9f1
loukue01 <Louis.Kueh@arm.com>
2020-09-23 15:32:56 +0100

    fixed currently playing to change automatically

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,6 +5,14 @@ import axios from "axios";
 import Player from "./Player.jsx";
 
 class LandingPage extends React.Component {
+  emptyItem = {
+    album: {
+      images: [{ url: "" }],
+    },
+    name: "",
+    artists: [{ name: "" }],
+    duration_ms: 0,
+  };
   componentDidMount() {
     this.interval = setInterval(
       () => this.getCurrentlyPlaying(this.props.token),
@@ -18,14 +26,7 @@ class LandingPage extends React.Component {
     super();
     this.state = {
       token: null,
-      item: {
-        album: {
-          images: [{ url: "" }],
-        },
-        name: "",
-        artists: [{ name: "" }],
-        duration_ms: 0,
-      },
+      item: this.emptyItem,
       is_playing: "Paused",
       progress_ms: 0,
     };
@@ -33,7 +34,7 @@ class LandingPage extends React.Component {
   }
 
   getCurrentlyPlaying(token) {
-    console.log("Inside currently playing");
+    console.log("Polling for currently playing");
     const getCurrentSpotify = axios.create({
       baseURL: "https://api.spotify.com",
       headers: { Authorization: "Bearer " + token },
@@ -43,19 +44,42 @@ class LandingPage extends React.Component {
       .get("/v1/me/player/currently-playing")
       .then((response) => {
         console.log(response);
-        this.setState({
-          item: response.data.item,
-          is_playing: response.data.is_playing,
-          progress_ms: response.data.progress_ms,
-        });
+        // If we are playing something
+        if ((response.status == 200)) {
+          this.setState({
+            item: response.data.item,
+            is_playing: response.data.is_playing,
+            progress_ms: response.data.progress_ms,
+          });
+        }else{
+          this.setState({
+            item: this.emptyItem,
+            is_playing: "Paused",
+            progress_ms: 0,
+          })
+        }
       })
       .catch((error) => {
-        console.error(error.response.data);
+        console.error(error.response);
       });
   }
 
   render() {
     const { classes } = this.props;
+    let placeHolder;
+    // console.log("STATE IS " + JSON.stringify(this.state.item));
+    if (this.state == this.emptyItem) {
+      // Empty image
+      placeHolder = <div>Please play a song on Spotify</div>;
+    } else {
+      placeHolder = (
+        <Player
+          item={this.state.item}
+          is_playing={this.state.is_playing}
+          progress_ms={this.state.progress_ms}
+        />
+      );
+    }
     return (
       <div>
         LandingPage
@@ -65,11 +89,7 @@ class LandingPage extends React.Component {
         <button onClick={() => this.getCurrentlyPlaying(this.props.token)}>
           Get current playing
         </button>
-        <Player
-          item={this.state.item}
-          is_playing={this.state.is_playing}
-          progress_ms={this.state.progress_ms}
-        />
+        {placeHolder}
       </div>
     );
   }
