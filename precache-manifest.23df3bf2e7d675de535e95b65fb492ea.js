6a460a0a834c907c1298c78f4276140629fdaae3
Louis Kueh <jovanhan2@gmail.com>
2020-11-17 17:59:55 +0000

    Updates

---
+++ precache-manifest.23df3bf2e7d675de535e95b65fb492ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13599bab174582d91183e5ea73543790",
+    "url": "/artist-guesser-spotify/index.html"
+  },
+  {
+    "revision": "c05e2b2df80ffa60a0cb",
+    "url": "/artist-guesser-spotify/static/css/2.60604e42.chunk.css"
+  },
+  {
+    "revision": "4d143d3f4b65a572c3d5",
+    "url": "/artist-guesser-spotify/static/css/main.97ea89cf.chunk.css"
+  },
+  {
+    "revision": "c05e2b2df80ffa60a0cb",
+    "url": "/artist-guesser-spotify/static/js/2.b928446b.chunk.js"
+  },
+  {
+    "revision": "653716e4be32844bed65585eb573e4b0",
+    "url": "/artist-guesser-spotify/static/js/2.b928446b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d143d3f4b65a572c3d5",
+    "url": "/artist-guesser-spotify/static/js/main.e045446a.chunk.js"
+  },
+  {
+    "revision": "f5bdac3d701c65486010",
+    "url": "/artist-guesser-spotify/static/js/runtime-main.2be2aa01.js"
+  }
+]);
