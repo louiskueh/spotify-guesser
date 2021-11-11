c8a9a78045ca48a061fb6bd4be15a500a6c48e60
Louis Kueh <jovanhan2@gmail.com>
2021-11-11 10:51:42 +0000

    feat: add pwa

---
+++ app/src/main/java/com/louiskueh/spotify_guesser/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.louiskueh.spotify_guesser.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
